07f2be56b41968228202994b9da26ab4ae44eafc
cyliong <liongtheprogrammer@gmail.com>
2020-09-14 18:45:32 +0800

    Toggle search mode

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -10,21 +10,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const Icon _searchIcon = Icon(Icons.search);
+  static const Icon _cancelIcon = Icon(Icons.cancel);
+
+  Icon _activeIcon;
+  Widget _titleBar;
+
+  @override
+  void initState() {
+    super.initState();
+    _activeIcon = _searchIcon;
+    _titleBar = Text(widget.title);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: _titleBar,
         actions: [
           IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
+            icon: _activeIcon,
+            onPressed: () {
+              setState(() {
+                if (_activeIcon.icon == Icons.search) {
+                  _activeIcon = _cancelIcon;
+                  _titleBar = _buildSearchBar();
+                } else {
+                  _activeIcon = _searchIcon;
+                  _titleBar = Text(widget.title);
+                }
+              });
+            },
           )
         ],
       ),
       body: const Center(
         child: Text("No weather data"),
       ),
+    );
+  }
+
+  TextField _buildSearchBar() {
+    return TextField(
+      autofocus: true,
+      cursorColor: Colors.lightBlueAccent,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: 'Search a location',
+      ),
+      textInputAction: TextInputAction.search,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+      ),
+      onSubmitted: (String text) {},
     );
   }
 }
