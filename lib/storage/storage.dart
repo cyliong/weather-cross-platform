98eff29bfcd7587d5f92c3cb076e39a7960b69e4
cyliong <liongtheprogrammer@gmail.com>
2020-10-19 19:56:17 +0800

    Implement a Storage class with singleton pattern

---
+++ lib/storage/storage.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather/model/weather.dart';
+
+class Storage {
+  static final _instance = Storage._internal();
+  factory Storage() => _instance;
+  Storage._internal();
+
+  static const String _latitudePreferencesKey = 'lat';
+  static const String _longitudePreferencesKey = 'lon';
+
+  final _prefsFuture = SharedPreferences.getInstance();
+
+  Future<Coordinates> getSavedCoordinates() async {
+    final prefs = await _prefsFuture;
+    return Coordinates(
+      latitude: prefs.getDouble(_latitudePreferencesKey),
+      longitude: prefs.getDouble(_longitudePreferencesKey),
+    );
+  }
+
+  void saveCoordinates(Coordinates coordinates) async {
+    final prefs = await _prefsFuture;
+    prefs.setDouble(_latitudePreferencesKey, coordinates.latitude);
+    prefs.setDouble(_longitudePreferencesKey, coordinates.longitude);
+  }
+}
