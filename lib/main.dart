31f15ac0724845d9b7b94068591eabb20745c2f8
cyliong <liongtheprogrammer@gmail.com>
2021-09-27 13:08:02 +0800

    Provide a const constructor for the App class

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:weather/page/home_page.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   static const String _title = 'Weather';
 
   @override
