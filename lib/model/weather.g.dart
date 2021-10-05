df3a73918bbd0a604e4cddbbb1f461ca6530176a
cyliong <liongtheprogrammer@gmail.com>
2021-10-05 18:09:32 +0800

    File updated by json_serializable

--- lib/model/weather.g.dart
+++ lib/model/weather.g.dart
@@ -6,34 +6,26 @@ part of 'weather.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Weather _$WeatherFromJson(Map<String, dynamic> json) {
-  return Weather(
-    city: json['name'] as String,
-    countryCode:
-        Weather._countryCodeFromSystemJson(json['sys'] as Map<String, dynamic>),
-    coordinates: Coordinates.fromJson(json['coord'] as Map<String, dynamic>),
-    mainData: MainData.fromJson(json['main'] as Map<String, dynamic>),
-    weatherData: Weather._weatherDataFromWeatherJson(json['weather'] as List),
-  );
-}
+Weather _$WeatherFromJson(Map<String, dynamic> json) => Weather(
+      city: json['name'] as String,
+      countryCode: Weather._countryCodeFromSystemJson(
+          json['sys'] as Map<String, dynamic>),
+      coordinates: Coordinates.fromJson(json['coord'] as Map<String, dynamic>),
+      mainData: MainData.fromJson(json['main'] as Map<String, dynamic>),
+      weatherData: Weather._weatherDataFromWeatherJson(json['weather'] as List),
+    );
 
-Coordinates _$CoordinatesFromJson(Map<String, dynamic> json) {
-  return Coordinates(
-    latitude: (json['lat'] as num).toDouble(),
-    longitude: (json['lon'] as num).toDouble(),
-  );
-}
+Coordinates _$CoordinatesFromJson(Map<String, dynamic> json) => Coordinates(
+      latitude: (json['lat'] as num).toDouble(),
+      longitude: (json['lon'] as num).toDouble(),
+    );
 
-MainData _$MainDataFromJson(Map<String, dynamic> json) {
-  return MainData(
-    temperature: (json['temp'] as num).toDouble(),
-    humidity: json['humidity'] as int,
-  );
-}
+MainData _$MainDataFromJson(Map<String, dynamic> json) => MainData(
+      temperature: (json['temp'] as num).toDouble(),
+      humidity: json['humidity'] as int,
+    );
 
-WeatherData _$WeatherDataFromJson(Map<String, dynamic> json) {
-  return WeatherData(
-    condition: json['main'] as String,
-    icon: json['icon'] as String,
-  );
-}
+WeatherData _$WeatherDataFromJson(Map<String, dynamic> json) => WeatherData(
+      condition: json['main'] as String,
+      icon: json['icon'] as String,
+    );
